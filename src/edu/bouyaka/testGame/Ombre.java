2ccf466506965dfa81fb6aac46d703a9b08311f4
valxx <val.t.t@hotmail.fr>
2013-04-30 12:09:35 +0200

    - Adaptation de jeu à la dernière version de test du moteur.

--- src/edu/bouyaka/testGame/Ombre.java
+++ src/edu/bouyaka/testGame/Ombre.java
@@ -5,10 +5,6 @@ import edu.bouyaka.engine.concreted.Item;
 
 public class Ombre extends Item {
 
-	public Ombre() {
-		type = "Ombre";
-	}
-
 	public void collideTo(Concrete E) {
 	}
 
--- src/edu/bouyaka/testGame/PlayButton.java
+++ src/edu/bouyaka/testGame/PlayButton.java
@@ -3,11 +3,8 @@ package edu.bouyaka.testGame;
 import edu.bouyaka.engine.concreted.Button;
 
 public class PlayButton extends Button {
-	public void leftClick(int x, int y) {
-		engine.state = "Play";
+	public void leftClick(int x, int y){
+		engine.state="play";
 		engine.Interface(1).enable(false);
-		engine.Interface(1).setVisible(false);
 	}
-
-
 }
